eb3d18e8ac643db2e4f08a38852381b49c2a303d
bel001 <rodrigoabel2801@gmail.com>
2026-04-15 09:11:01 -0500

    feat(iac): agregar red docker y servicio mysql

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,2 +1,26 @@
-variable "web_port" {}
-variable "api_port" {}
+variable "web_port" {
+  type = map(number)
+
+  default = {
+    localhost = 4001
+    dev       = 5001
+  }
+}
+
+variable "api_port" {
+  type = map(number)
+
+  default = {
+    localhost = 4002
+    dev       = 5002
+  }
+}
+
+variable "db_port" {
+  type = map(number)
+
+  default = {
+    localhost = 4003
+    dev       = 5003
+  }
+}
--- iac/web.tf
+++ iac/web.tf
@@ -1,19 +1,111 @@
-resource "docker_container" "web" {
-  name  = "web-${terraform.workspace}-01"
-  image = "lab/web"
+locals {
+  environment = terraform.workspace == "default" ? "localhost" : terraform.workspace
+  web_context = abspath("${path.module}/../src/web")
+  api_context = abspath("${path.module}/../src/api")
+  db_name     = "lab_db"
+  db_user     = "lab_user"
+  db_password = "lab_password"
 
-   ports {
-    internal = "80"
-    external = var.web_port[terraform.workspace]
+  web_source_hash = sha1(join("", [
+    for file in sort(fileset(local.web_context, "**")) :
+    filesha256("${local.web_context}/${file}")
+  ]))
+  api_source_hash = sha1(join("", [
+    for file in sort(fileset(local.api_context, "**")) :
+    filesha256("${local.api_context}/${file}")
+  ]))
+}
+
+resource "docker_network" "app" {
+  name = "lab-${local.environment}-network"
+}
+
+resource "docker_image" "web" {
+  name         = "lab/web"
+  keep_locally = true
+
+  triggers = {
+    source_hash = local.web_source_hash
+  }
+
+  build {
+    context    = local.web_context
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_container" "web" {
+  name  = "web-${local.environment}-01"
+  image = docker_image.web.image_id
+
+  ports {
+    internal = 80
+    external = var.web_port[local.environment]
+  }
+}
+
+resource "docker_image" "api" {
+  name         = "lab/api"
+  keep_locally = true
+
+  triggers = {
+    source_hash = local.api_source_hash
+  }
+
+  build {
+    context    = local.api_context
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_image" "db" {
+  name         = "mysql:8.4"
+  keep_locally = true
+}
+
+resource "docker_container" "db" {
+  name  = "db-${local.environment}-01"
+  image = docker_image.db.image_id
+
+  env = [
+    "MYSQL_DATABASE=${local.db_name}",
+    "MYSQL_USER=${local.db_user}",
+    "MYSQL_PASSWORD=${local.db_password}",
+    "MYSQL_ROOT_PASSWORD=${local.db_password}",
+  ]
+
+  ports {
+    internal = 3306
+    external = var.db_port[local.environment]
+  }
+
+  networks_advanced {
+    name    = docker_network.app.name
+    aliases = ["db"]
   }
 }
 
 resource "docker_container" "api" {
-  name  = "api-${terraform.workspace}-01"
-  image = "lab/api"
+  name  = "api-${local.environment}-01"
+  image = docker_image.api.image_id
 
-   ports {
-    internal = "3000"
-    external = var.api_port[terraform.workspace]
+  env = [
+    "DB_HOST=db",
+    "DB_PORT=3306",
+    "DB_NAME=${local.db_name}",
+    "DB_USER=${local.db_user}",
+    "DB_PASSWORD=${local.db_password}",
+  ]
+
+  ports {
+    internal = 3000
+    external = var.api_port[local.environment]
   }
+
+  networks_advanced {
+    name    = docker_network.app.name
+    aliases = ["api01"]
+  }
+
+  depends_on = [docker_container.db]
 }
